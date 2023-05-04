73284bd116d1fd167b39c4568e0461c0e7ac8459
ECG301997 <eduardo301997@gmail.com>
2023-05-04 15:34:53 -0500

    first commit

---
+++ BASES/app.js
@@ -0,0 +1,17 @@
+"use strict";
+// const msg:string = 'Hola Mundo'
+// const hero = {
+//     name:'Iron Man',
+//     age: 45
+// }
+// hero.age = 50
+// console.log(hero.age+1);
+// const a:number = 10;
+// let b:string;
+// function sayHello(msg:string){
+//     console.log(msg + '' + 'Eduardo');    
+// }
+(() => {
+    const a = 10;
+    console.log(a);
+})();
